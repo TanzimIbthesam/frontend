a2f0ed3588aa2f057971ebdf8582effbfb66a5e3
TanzimIbthesam <35595427+TanzimIbthesam@users.noreply.github.com>
2019-05-02 18:20:22 +0600

    Add files via upload

---
+++ frontend/projects/slidelogin/main.js
@@ -0,0 +1,33 @@
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
+
+// signUpButton.addEventListener('click', () => {
+// 	container.classList.add("right-panel-active");
+// });
+
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
+
+
+// const signUpButton=document.getElementById('signUp');
+// const signInButton=document.getElementById('signIn');
+// const container=document.getElementById('container');
+
+// signUpButton.addEventListener('click',()=>{ 
+// container.classList.add('right-panel-active');
+// });
+
+// signUpButton.addEventListener('click',()=>{ 
+//     container.classList.add('right-panel-active');
+//     });
+// //
+// /*
+// Move sign in to the right
+
+
+
+
+// */
+
